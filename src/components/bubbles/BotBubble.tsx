847acc557953491e59a1599a0d0081ecf58297a8
OmniChatEmbed <Support@omnipro-bh.com>
2024-02-17 12:15:11 +0300

    17Feb

--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -70,7 +70,7 @@ export const BotBubble = (props: Props) => {
       </Show>
       <span
         ref={botMessageEl}
-        class="px-4 py-2 ml-2 max-w-full chatbot-host-bubble prose"
+        class="px-4 py-2 ml-2 max-w-full chatbot-host-bubble"
         data-testid="host-bubble"
         style={{
           'background-color': props.backgroundColor ?? defaultBackgroundColor,
